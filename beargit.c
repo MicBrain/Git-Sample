373b7f3787e6d1d96eefd4e0adc995677fa0926c
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2015-03-13 22:07:58 -0700

    Update beargit.c

--- beargit.c
+++ beargit.c
@@ -13,7 +13,6 @@
  *   and written to stderr. We catch some additional errors for you in main.c.
  * - Output to stdout needs to be exactly as specified in the function description.
  * - Only edit this file (beargit.c)
- * - You are given the following helper functions:
  *   * fs_mkdir(dirname): create directory <dirname>
  *   * fs_rm(filename): delete file <filename>
  *   * fs_mv(src,dst): move file <src> to <dst>, overwriting <dst> if it exists
